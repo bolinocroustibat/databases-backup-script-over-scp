de40f52410d22e4f4a2fef7c1ae5aa6ab5e76a64
Adrien Carpentier <moi@adriencarpentier.com>
2019-04-04 10:15:39 +0200

    Update example_settings.py

--- example_settings.py
+++ example_settings.py
@@ -8,10 +8,10 @@ MYSQL_USER = '' # make sure this MySQL user has enough privileges to take all da
 MYSQL_USER_PASSWORD = ''
 
 ## Local setting
-LOCAL_PATH = '/root/backup-sql/dumps/' # full local path where dumps will be saved, with trailing slash.
-LOGFILE = '/root/backup-sql/log-last-script.log' # full path to log file.
+LOCAL_PATH = '/root/databases-backup-script-over-scp/dumps/' # full local path where dumps will be saved, with trailing slash.
+LOGFILE = '/root/databases-backup-script-over-scp/log-last-script.log' # full path to log file.
 
 ### Remote settings
-REMOTE_URL = '' # leave blank if you don't want to save remotely
+REMOTE_URL = '' # leave blank if you don't want to save remotely.
 REMOTE_USER = '' # you need to be authorized on remote with your user SSH keys.
 REMOTE_PATH = '/home/backup-sql/' # full remote path where dumps will be saved.
